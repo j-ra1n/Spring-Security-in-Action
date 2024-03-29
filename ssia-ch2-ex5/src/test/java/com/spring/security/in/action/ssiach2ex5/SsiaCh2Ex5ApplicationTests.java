e214a459c1747c65f5626643051f806b2e72c2cf
j-ra1n <j_ra1n@naver.com>
2024-03-29 18:26:22 +0900

    Setting(#5): Project Setting

---
+++ ssia-ch2-ex5/src/test/java/com/spring/security/in/action/ssiach2ex5/SsiaCh2Ex5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.in.action.ssiach2ex5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh2Ex5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
